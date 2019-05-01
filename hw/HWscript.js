f5d0200f97db931c0fa421e460027bc0688308e6
Asset <asetik93@yandex.kz>
2019-05-01 16:10:34 +0600

    lesson 2 and hw part 2

--- hw/HWscript.js
+++ hw/HWscript.js
@@ -1,20 +1,69 @@
 'use strict'
 
 
-var money = +prompt("Ваш бюджет на месяц"),
-    time = prompt("Введите дату в формате YYYY-MM-DD", "2019-04-"),
-    expenseName = prompt("Введите обязательную статью расходов в этом месяце", "Bread"),
-    expenseCost = +prompt("Во сколько обойдется?");
+let money = +prompt("Ваш бюджет на месяц", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "2019-04-");
 
-var appData = {
+let appData = {
     budget: money,
-    timeData: time,
-    expenses: {
-        expenseName: expenseCost
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
+    timeData: time,
     saving: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+    
+    if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null
+        && a != "" && b != "" && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        i = 0;
+    }
 }
 
-alert("Ваш бюджет на 1 день = " + ((money-expenseCost)/30));
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ваш бюджет на 1 день: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
+
+// let i = 0
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
+//     if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null
+//         && a != "" && b != "" && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     } else {
+//         i = 0;
+//     }
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
+//     if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null
+//         && a != "" && b != "" && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     } else {
+//         i = 0;
+//     }   
+//     i++;
+// } while (i < 2);
---
+++ hw/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+// let i = 0
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
+//     if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null
+//         && a != "" && b != "" && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     }
+//     i++;
+// }
