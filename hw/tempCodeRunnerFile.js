3b74bbd626253e0f4243d3d662e24ab0134a3acb
Asset <asetik93@yandex.kz>
2019-05-15 19:46:17 +0600

    lesson 3 and hw part 3

--- hw/tempCodeRunnerFile.js
+++ hw/tempCodeRunnerFile.js
@@ -1,11 +1,5 @@
-// let i = 0
-// while (i < 2) {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt("Во сколько обойдется?", "");
-//     if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null
-//         && a != "" && b != "" && a.length < 50) {
-//         console.log("done");
-//         appData.expenses[a] = b;
-//     }
-//     i++;
-// }
+var user = {};
+user.name = "Вася";
+user.surname = "Петров";
+user.name = "Сергей";
+console.log(user);
